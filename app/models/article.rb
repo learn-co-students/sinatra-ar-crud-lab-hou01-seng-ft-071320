6bfcc0685f79172b33015f93ee08476675183e7a
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-08-06 15:16:17 -0500

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,16 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+    # attr_accessor :title, :content
+    # @@all = []
+
+    # def initialize(hash)
+    #     @title = hash[:title]
+    #     @content = hash[:content]
+    #     @@all << self.all
+    # end
+
+    # def self.all
+    #     @@all
+    # end
 end
